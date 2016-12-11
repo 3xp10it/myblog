d4b13fb0a52ceb67813debcb109aff28cb53fef9
3xp10it <quanyechavshuo@gmail.com>
2016-12-11 11:24:04 +0800

    update backup

--- jquery-jplayer/js/follow.js
+++
@@ -1,4 +0,0 @@
-/*document.writeln("<div style=\"width:252px;margin:10px auto;\"><iframe width=\"250\" height=\"100\" class=\"share_self\"  frameborder=\"0\" scrolling=\"no\"")
-document.writeln("src=\"https://widget.weibo.com/weiboshow/index.php?")
-document.writeln("language=&width=250&height=100&fansRow=2&ptype=1&speed=0&skin=1&")
-document.writeln("isTitle=0&noborder=1&isWeibo=0&isFans=0&uid=2832482174&verifier=d71718ca&dpc=1\"></iframe></div>")*/
