0feaa9c4bd3a6edb7d95e860804a309d4f4cc549
3xp10it <quanyechavshuo@gmail.com>
2017-03-12 02:05:58 +0800

    update backup

--- 1.js
+++
@@ -1,4 +0,0 @@
-<!-- try to add style like http://tools88.com 
-<script type="text/javascript" src="{{ site.baseurl }}/js/navi.js"></script>
--->
-
--- _posts/111.py
+++
@@ -1,6 +0,0 @@
-import re
-string="Hello,World! 你好世界"
-if re.search("你好.*",string):
-    print("good,matched")
-else:
-    print("bad,no matched")
