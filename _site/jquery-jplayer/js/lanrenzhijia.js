df24a2b7b19c643ec7d2add1853851c58656bbe4
3xp10it <quanyechavshuo@gmail.com>
2017-08-11 21:13:36 +0800

    update backup

--- _site/jquery-jplayer/js/lanrenzhijia.js
+++ _site/jquery-jplayer/js/lanrenzhijia.js
@@ -199,7 +199,13 @@ $(document).ready(function(){
       mp3:"http://oiqwnrsx4.bkt.clouddn.com/%E7%84%9A%E6%83%85.mp3",
       //oga:"https://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg",
       poster: "https://33.media.tumblr.com/bf9dc125a47dcca91ce5b3575bc3ba92/tumblr_nbmb3j8nU51sq3g2zo1_500.png"
-    }];
+    },{
+      title:"铁血丹心",
+      artist:"射雕英雄传",
+      mp3:"http://oiqwnrsx4.bkt.clouddn.com/%E9%93%81%E8%A1%80%E4%B8%B9%E5%BF%83.mp3",
+      //oga:"https://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg",
+      poster: "https://33.media.tumblr.com/bf9dc125a47dcca91ce5b3575bc3ba92/tumblr_nbmb3j8nU51sq3g2zo1_500.png"
+    ];
 
   var cssSelector = {
     jPlayer: "#jquery_jplayer",
