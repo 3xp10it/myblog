8a336a444bcdf881ed98e0fd415fa90a781c124e
3xp10it <quanyechavshuo@gmail.com>
2017-12-20 15:00:58 +0800

    update backup

--- _site/updatelocal.sh
+++ _site/updatelocal.sh
@@ -1,1 +1,2 @@
-pkill jekyll;sleep 3 && cp index.html tmp.html && cp index_bak.html index.html && sh -c "rm -r _site/ & jekyll serve --watch & sleep 3 && exit" && cp _site/index.html index2.html && cp tmp.html index.html && echo congratulations! commands execute ok to here!
+cd ~/myblog && kill -9 $(ps -a | egrep "jekyll" | egrep -o "\d+" | head -n 1);sleep 3 && cp index.html tmp.html && cp index_bak.html index.html && sh -c "rm -r _site/ & jekyll serve -H 0.0.0.0 --incremental --watch && exit" && cp _site/index.html index2.html && cp tmp.html index.html && echo congratulations! commands execute ok to here!
+
